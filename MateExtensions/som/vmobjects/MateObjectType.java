632b284a52462a9c8a320138d0cccdd68eddd74a
Guido Chari <charig@gmail.com>
2017-02-21 16:01:00 -0300

    Output the class of objects when debugging

--- MateExtensions/som/vmobjects/MateObjectType.java
+++ MateExtensions/som/vmobjects/MateObjectType.java
@@ -1,6 +1,10 @@
 package som.vmobjects;
 
 import som.interop.DynamicObjectInteropMessageResolutionForeign;
+import som.vm.constants.Nil;
+import som.vmobjects.SObjectLayoutImpl.SObjectType;
+
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.interop.ForeignAccess;
 import com.oracle.truffle.api.object.DynamicObject;
 import com.oracle.truffle.api.object.ObjectType;
@@ -10,5 +14,25 @@ public class MateObjectType extends ObjectType {
   @Override
   public ForeignAccess getForeignAccessFactory(DynamicObject object) {
     return DynamicObjectInteropMessageResolutionForeign.createAccess();
-}
+  }
+  
+  @Override
+  @TruffleBoundary
+  public String toString(DynamicObject object) {
+    return super.toString(object) +
+    "\nclass:" + SClass.getName(SObject.getSOMClass(object));
+  }
+  
+  public static class MateReflectiveObjectType extends SObjectLayoutImpl.SObjectType {
+    public MateReflectiveObjectType(DynamicObject klass) {
+      super(klass);
+    }
+
+    @Override
+    @TruffleBoundary
+    public String toString(DynamicObject object) {
+      return super.toString(object) +
+      "\nhas metaobject:" + String.valueOf(SReflectiveObject.getEnvironment(object) != Nil.nilObject);
+    }
+  }
 }
