5ae726a0c850f422f150b04faa8c2d98dec230f5
Guido Chari <charig@gmail.com>
2017-07-14 18:48:20 -0300

    Add dyn metrics test and reorganize tests folder

---
+++ tests/dym/test.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+## Exit on first error
+if [ "$1" != "update" ]
+then
+  # quit on first error
+  set -e
+fi
+
+## Determine absolute path of script
+pushd `dirname $0` > /dev/null
+SCRIPT_PATH=`pwd`
+popd > /dev/null
+
+SOM_DIR=$SCRIPT_PATH/../..
+
+## create folder for new results
+mkdir -p $SCRIPT_PATH/results/
+
+## extract expected results
+tar xf $SCRIPT_PATH/expected-results.tar.bz2 -C $SCRIPT_PATH/
+
+function runBenchmark {
+  BENCH=$1
+  HARNESS="$SOM_DIR/som -dm -Ddm.metrics=$SCRIPT_PATH/results/$BENCH \
+    -G BenchmarkHarness"
+  echo $HARNESS $@
+  $HARNESS $@
+  
+  diff -r $SCRIPT_PATH/expected-results/$BENCH $SCRIPT_PATH/results/$BENCH
+}
+
+## Language Features
+runBenchmark Fibonacci    1 0 2
+runBenchmark Dispatch     1 0 2
+runBenchmark Loop         1 0 2
+runBenchmark Recurse      1 0 2
+runBenchmark IntegerLoop  1 0 2
+runBenchmark FieldLoop    1 0 1
+
+## Sorting
+runBenchmark QuickSort  1 0 2
+runBenchmark TreeSort   1 0 2
+runBenchmark BubbleSort 1 0 2
+
+## General
+runBenchmark Richards    1 0 1
+runBenchmark DeltaBlue   1 0 2
+runBenchmark Mandelbrot  1 0 3
+runBenchmark NBody       1 0 3
+runBenchmark Json        1 0 1
+runBenchmark GraphSearch 1 0 1
+# runBenchmark PageRank    1 0 1   # takes too long
+runBenchmark Fannkuch    1 0 4
+runBenchmark List        1 0 1
+runBenchmark Bounce      1 0 1
+runBenchmark Permute     1 0 5
+runBenchmark Queens      1 0 5
+runBenchmark Storage     1 0 2
+runBenchmark Sieve       1 0 2
+runBenchmark Towers      1 0 2
+
+if [ "$1" = "update" ]
+then
+  ## move old results out of the way, and new results to expected folder
+  rm -Rf $SCRIPT_PATH/old-results
+  mv $SCRIPT_PATH/expected-results $SCRIPT_PATH/old-results
+  mv $SCRIPT_PATH/results $SCRIPT_PATH/expected-results
+  ## update the archive
+  tar cjf $SCRIPT_PATH/expected-results.tar.bz2 -C $SCRIPT_PATH expected-results
+fi
