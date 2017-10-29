f3685189e72439329de79c825603fd3ba978562d
Guido Chari <charig@gmail.com>
2017-10-29 20:23:53 -0300

    Adapt dispatch guard of version with environments in the object

--- src/som/interpreter/nodes/dispatch/DispatchGuard.java
+++ src/som/interpreter/nodes/dispatch/DispatchGuard.java
@@ -1,13 +1,10 @@
 package som.interpreter.nodes.dispatch;
 
-import som.vmobjects.SBlock;
-import som.vmobjects.SObject;
-import som.vmobjects.SObjectLayoutImpl.SObjectType;
-
-import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.nodes.InvalidAssumptionException;
 import com.oracle.truffle.api.object.DynamicObject;
-import com.oracle.truffle.api.object.Shape;
+
+import som.vmobjects.SBlock;
+import som.vmobjects.SReflectiveObjectEnvInObj;
 
 
 public abstract class DispatchGuard {
@@ -23,7 +20,7 @@ public abstract class DispatchGuard {
     }
 
     if (obj instanceof DynamicObject) {
-      return new CheckSObject(((DynamicObject) obj).getShape());
+      return new CheckSObject((DynamicObject) obj);
     }
 
     if (obj instanceof SBlock) {
@@ -79,25 +76,16 @@ public abstract class DispatchGuard {
   }
 
   private static final class CheckSObject extends DispatchGuard {
-    private final Shape expected;
     private final DynamicObject klass;
 
-    CheckSObject(final Shape expected) {
-      this.expected = expected;
-      this.klass = ((SObjectType) (expected.getObjectType())).getKlass();
+    CheckSObject(final DynamicObject object) {
+      this.klass = SReflectiveObjectEnvInObj.getSOMClass(object);
     }
 
   @Override
   public boolean entryMatches(final Object obj) throws InvalidAssumptionException {
-    if (!expected.isValid()) {
-      CompilerDirectives.transferToInterpreter();
-      throw new InvalidAssumptionException();
-    }
     return obj instanceof DynamicObject &&
-        (
-            (((DynamicObject) obj).getShape() == expected)
-            || (SObject.getSOMClass((DynamicObject) obj) == klass)
-        );
+        SReflectiveObjectEnvInObj.getSOMClass((DynamicObject) obj) == klass;
     }
   }
 
